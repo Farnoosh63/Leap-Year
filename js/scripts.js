1b468e621157962659f0b3f215378f7251329ef9
farnoosh <farnoosh@pdx.edu>
2016-02-03 20:47:38 -0800

    add bootstrap and update jquery file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,29 @@
 var leapYear = function(year) {
-  if((year % 4 === 0) && (year % 100 !== 0)){
+  if(((year >0 ) && (year % 4 === 0) && (year % 100 !== 0))
+   || ((year >0 ) && (year % 400 === 0))){
       return true;
-  }else{
+  }else if (isNaN(year)){
+    alert("not a number")
+  return false;
+}else {
   return false;
   }
 };
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (result === false) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -10,4 +10,12 @@ describe('leapYear', function() {
   it("is true for years divisible by 100", function() {
     expect(leapYear(1900)).to.equal(false);
   });
+
+  it("is true for years divisible by 400", function() {
+    expect(leapYear(2000)).to.equal(true);
+  });
+
+  it("is false for input that are negative", function() {
+    expect(leapYear(-2016)).to.equal(false);
+  });
 });
